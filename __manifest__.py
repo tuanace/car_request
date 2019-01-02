798fdc6534e638b17b5c3f1572142c77b6d42582
Nguyen Anh Tuan <tuanace@gmail.com>
2019-01-02 10:53:30 +0700

    [IMP]improve the module by adding more detail

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,7 @@
     'name': "Employee Car Requestt",
 
     'summary': """
-        This is shot description 
-        and this can have multiple line""",
+        Request a car and get the approval""",
 
     'description': """
         The module purpose is for learning creating a odoo module
@@ -16,18 +15,18 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','hr','fleet',],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],cd
+    ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
